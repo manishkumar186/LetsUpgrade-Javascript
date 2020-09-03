636e904202c0ba8f6af0df9520d228deeb6fb12d
Manish Kumar <65070286+manishkumar186@users.noreply.github.com>
2020-09-03 22:52:48 +0530

    Add files via upload

---
+++ Day 2/question4.js
@@ -0,0 +1,10 @@
+console.log("load");
+
+const list = ["harry","jack","scott","stive","smith","warner"];
+
+list.forEach(element => {
+    if(element.includes('a')){
+        console.log(element)
+    }
+    
+});
